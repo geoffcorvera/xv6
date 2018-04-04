789db9fce74b8b79487a941d33593ea223333c65
Geoffrey Corvera <gcorvera@pdx.edu>
2018-04-03 18:14:11 -0700

    syscall.c: added syscallnames[] array

--- syscall.c
+++ syscall.c
@@ -100,6 +100,33 @@ extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_halt(void);
 
+#ifdef PRINT_SYSCALLS
+static char *syscallnames[] = {
+[SYS_fork]      "fork",
+[SYS_exit]      "exit",
+[SYS_wait]      "wait",
+[SYS_pipe]      "pipe",
+[SYS_read]      "read",
+[SYS_kill]      "kill",
+[SYS_exec]      "exec",
+[SYS_fstat]     "fstat",
+[SYS_chdir]     "chdir",
+[SYS_dup]       "dup",
+[SYS_getpid]    "getpid",
+[SYS_sbrk]      "sbrk",
+[SYS_sleep]     "sleep",
+[SYS_uptime]    "uptime",
+[SYS_open]      "open",
+[SYS_write]     "write",
+[SYS_mknod]     "mknod",
+[SYS_unlink]    "unlink",
+[SYS_link]      "link",
+[SYS_mkdir]     "mkdir",
+[SYS_close]     "close",
+[SYS_halt]      "halt"
+};
+#endif
+
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
 [SYS_exit]    sys_exit,
