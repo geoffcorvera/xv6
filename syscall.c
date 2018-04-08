35aca69cccebd0cda648d1ee2995e71b74e5ac56
Geoffrey Corvera <gcorvera@pdx.edu>
2018-04-08 16:14:34 -0700

    moved syscallnames[] down some lines

--- syscall.c
+++ syscall.c
@@ -103,34 +103,6 @@ extern int sys_halt(void);
 extern int sys_date(void);
 #endif
 
-#ifdef PRINT_SYSCALLS
-static char *syscallnames[] = {
-[SYS_fork]      "fork",
-[SYS_exit]      "exit",
-[SYS_wait]      "wait",
-[SYS_pipe]      "pipe",
-[SYS_read]      "read",
-[SYS_kill]      "kill",
-[SYS_exec]      "exec",
-[SYS_fstat]     "fstat",
-[SYS_chdir]     "chdir",
-[SYS_dup]       "dup",
-[SYS_getpid]    "getpid",
-[SYS_sbrk]      "sbrk",
-[SYS_sleep]     "sleep",
-[SYS_uptime]    "uptime",
-[SYS_open]      "open",
-[SYS_write]     "write",
-[SYS_mknod]     "mknod",
-[SYS_unlink]    "unlink",
-[SYS_link]      "link",
-[SYS_mkdir]     "mkdir",
-[SYS_close]     "close",
-[SYS_halt]      "halt",
-[SYS_date]      "date"
-};
-#endif
-
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
 [SYS_exit]    sys_exit,
@@ -160,6 +132,33 @@ static int (*syscalls[])(void) = {
 };
 
 // put data structure for printing out system call invocation information here
+#ifdef PRINT_SYSCALLS
+static char *syscallnames[] = {
+[SYS_fork]      "fork",
+[SYS_exit]      "exit",
+[SYS_wait]      "wait",
+[SYS_pipe]      "pipe",
+[SYS_read]      "read",
+[SYS_kill]      "kill",
+[SYS_exec]      "exec",
+[SYS_fstat]     "fstat",
+[SYS_chdir]     "chdir",
+[SYS_dup]       "dup",
+[SYS_getpid]    "getpid",
+[SYS_sbrk]      "sbrk",
+[SYS_sleep]     "sleep",
+[SYS_uptime]    "uptime",
+[SYS_open]      "open",
+[SYS_write]     "write",
+[SYS_mknod]     "mknod",
+[SYS_unlink]    "unlink",
+[SYS_link]      "link",
+[SYS_mkdir]     "mkdir",
+[SYS_close]     "close",
+[SYS_halt]      "halt",
+[SYS_date]      "date"
+};
+#endif
 
 void
 syscall(void)
