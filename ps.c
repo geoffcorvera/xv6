ff74e132c478bb8fbd450ca0da80ad6a7bf34455
Geoffrey Corvera <gcorvera@pdx.edu>
2018-04-18 17:47:24 -0700

    Started ps user program

--- ps.c
+++ ps.c
@@ -1,10 +1,25 @@
 #ifdef CS333_P2
 #include "types.h"
+#include "uproc.h"
 #include "user.h"
+
+#define MAXPROC 16 // arbitrary value
+
 int
 main(void)
 {
-  printf(1, "Not imlpemented yet.\n");
+  struct uproc *p;
+  struct uproc *ptable = malloc(MAXPROC * sizeof(struct uproc));
+
+  printf(1,
+      "PID\tUID\tGID\tPPID\tElapsed Time\tCPU Time\tState\tSize\tName\n");
+
+  ptable[0] = (struct uproc){1,1,1,1,1,1,"Runnable",1,"Fake Proc"};
+  p = ptable;
+  printf(1, "PID: %d\n", p->pid);
+
+  free(ptable);
+
   exit();
 }
 #endif
